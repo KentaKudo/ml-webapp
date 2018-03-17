1173ae0614a344d5763e644af047d6a778cd2fd9
Kenta Kudo <yd37574@gmail.com>
2018-03-17 12:03:08 +0000

    fix: filepath

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,10 @@
+import os
 import pickle
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_datasets():
-  with open("jeans.pkl", 'rb') as f:
+  with open(os.path.dirname(__file__)+"/jeans.pkl", 'rb') as f:
     datasets = pickle.load(f)
     X = [v['x'] for v in datasets]
     y = [v['y'] for v in datasets]
